0a27c49a6d66d34c231087e7261422faa3fccabb
lichterberci <lichterberci@gmail.com>
2022-10-24 10:34:55 +0200

    Add project files.

---
+++ DerivativeCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿public partial class Program
+{
+	public static void Main ()
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
+
+public abstract class Node
+{
+
+}
+
+public sealed class Constant : Node
+{
+	double value;
+}
+
+public sealed class Variable : Node
+{
+	char name;
+}
+
+enum OperatorType
+{
+	Add, Sub, Mult, Div, Pow, Sin, Cos, Tan, Log
+}
+
+public sealed class Operator : Node
+{
+	OperatorType type;
+}
