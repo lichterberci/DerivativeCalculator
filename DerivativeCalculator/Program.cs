36fc0cb5b6634e6f5cff12c6623224345d5425ad
szg1 <63415246+szg1@users.noreply.github.com>
2023-02-10 21:59:07 +0100

    Update Program.cs

--- DerivativeCalculator/Program.cs
+++ DerivativeCalculator/Program.cs
@@ -11,136 +11,26 @@ public partial class Program
 
 		//DerivativeManager.DifferentiateFromConsole();
 
-		string result = DerivativeManager.DifferentiateString("x|x^2|+abs(2x)", out _, out _, out _, out _, out _);
-		//Console.WriteLine(result);
-
-		//return;
-
-		//TreeNode tree = new Div(
-		//	new Mult(
-		//		new Mult(
-		//			  new Constant(-1),
-		//			  new Constant(2)
-		//		  ),
-		//		new Variable('x')
-		//	),	
-		//	new Variable('y')
-		//);
-
-		//tree.PrintToConsole();
-
-		//tree = tree.Simplify(SimplificationParams.Default);
-		////tree = tree.GetSimplestForm(SimplificationParams.Default);
-
-		//tree.PrintToConsole();
+		//TreeNode tree = 
+		//TreeNode tree = new Cos(new Add(new Variable('a'), new Variable('b')));
+        //x^(logx/loga)
+        //TreeNode tree = new Pow(new Variable('x'),new Div(new Log(new Variable('x')),new Log(new Variable('a'))));
+        //2sinxcosx
+        //TreeNode tree = new Mult(new Mult(new Sin(new Variable('x')),new Cos(new Variable('x'))),new Constant(2));
+        //cosacosb-sinasinb
+        //TreeNode tree = new Sub(new Mult(new Cos(new Variable('a')),new Cos(new Variable('b'))), new Mult(new Sin(new Variable('a')), new Sin(new Variable('b'))));
+        //ln(x*a), ln(x/a), ln(a*b)
+        //TreeNode tree = new Ln(new Mult(new Variable('x'),new Variable('a')));
+        //TreeNode tree = new Ln(new Div(new Variable('x'), new Variable('a')));
+        //TreeNode tree = new Ln(new Mult(new Variable('a'), new Variable('b')));
 
 
-		//var left = new Mult(
-		//	new Div(
-		//		new Variable('a'),
-		//		new Constant(2)
-		//	),
-		//	new Variable('b')
-		//);
 
-		//var right = new Div(
-		//	new Mult(
-		//		new Variable('a'),
-		//		new Variable('b')
-		//	),
-		//	new Constant(2)
-		//);
+        Console.WriteLine(TreeUtils.CollapseTreeToString(tree));
 
-		//left.PrintToConsole();
-		//right.PrintToConsole();
 
-		//Console.WriteLine(TreeUtils.MatchPattern(left, right, out _));
+        tree = TreeUtils.GetSimplestForm(tree);
 
-		//const int maxIter = 2000;
-
-		//for (int i = 0; i < maxIter; i++)
-		//{
-		//	if (i % 100 == 0)
-		//		Console.WriteLine($"{i}/{maxIter}");
-
-		//	var exercise = ExerciseGenerator.GenerateRandomTree(
-		//		DifficultyMetrics.Easy,
-		//		new SimplificationParams(
-		//			  'x'
-		//			)
-		//	);
-
-		//	DerivativeManager.DifferentiateTree(exercise, 'x', out _, out _, out _, out _, null);
-
-		//	exercise.PrintToConsole();
-		//	//exercise.Diff('x').PrintToConsole();
-		//}
-
-		//var list = new List<TreeNode>()
-		//{
-		//	new Variable('b'),
-		//	new Constant(3),
-		//	new Variable('c'),
-		//	new Pow(new Variable('a'), new Variable('v')),
-		//	new Mult(new Variable('j'), new Variable('v')),
-		//	new Constant(1),
-		//	new Variable('a'),
-		//	new Variable('g'),
-		//	new Variable('j'),
-		//	new Variable('r'),
-		//	new Variable('j'),
-		//	new Variable('x'),
-		//};
-
-		//list = TreeUtils.SortNodesByVarNames(list, 'x');
-
-		//list.ForEach(n => Console.WriteLine(n));
-		//TreeNode tree = new Mult(
-		//	new Sin(new Variable('a')),
-		//	new Pow(
-		//		new Cos(new Variable('a')),
-		//		new Constant(-1)
-		//	)
-		//);
-
-		////tree = new Mult(new Constant(1), new Mult(new Constant(-21), new Variable('x')));
-
-		//Console.WriteLine(TreeUtils.CollapseTreeToString(tree));
-
-		//TreeUtils.PrintTree(tree);
-
-		//tree = TreeUtils.GetSimplestForm(tree);
-
-		//Console.WriteLine(TreeUtils.CollapseTreeToString(tree));
-
-		//TreeUtils.PrintTree(tree);
-
-		//var sw = new Stopwatch();
-
-		//const int iters = 10;
-
-		//double[] times = new double[iters];
-
-		//for (int i = 0; i < iters; i++)
-		//{
-		//	Console.WriteLine($"{i + 1}/{iters}");
-
-		//	sw.Start();
-
-		//	var tree = ExerciseGenerator.GenerateRandomTree(DifficultyMetrics.Hardcore);
-
-		//	tree.ToLatexString();
-
-		//	sw.Stop();
-
-		//	times[i] = sw.ElapsedMilliseconds / 1000;
-
-		//	sw.Reset();
-		//}
-
-		//Console.WriteLine($"min: {times.Min()}s");
-		//Console.WriteLine($"max: {times.Max()}s");
-		//Console.WriteLine($"avg: {times.Sum() / iters}s");
-		//Console.WriteLine($"std: {Math.Sqrt(times.Select(t => Math.Pow(t - (times.Sum() / iters), 2)).ToArray().Sum())}s");
+        Console.WriteLine(TreeUtils.CollapseTreeToString(tree));
 	}
 }
